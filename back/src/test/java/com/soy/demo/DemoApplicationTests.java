a6e1092eaf795aacd06759e23e01071918ab9915
Bot-997 <39685852@qq.com>
2023-08-10 09:39:15 +0800

    初始化

---
+++ back/src/test/java/com/soy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
